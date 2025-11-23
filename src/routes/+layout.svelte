c32a4ff039fdcd8919baed9134f7ab7b3f41204a
Steve Strebl <60321901+stevestrebl@users.noreply.github.com>
2025-11-23 12:47:24 -0800

    add google analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,15 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-9HN938D44L"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'G-9HN938D44L');
+	</script>
 </svelte:head>
 
 {@render children()}
